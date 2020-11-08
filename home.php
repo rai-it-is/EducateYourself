f32b5bdaf802f70c213869764450a4244819d96e
Saurabh Rai <saurabh1998rai@gmail.com>
2020-11-08 17:31:18 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+
+<p> <img id="rai" src="data1\images\ait5.jpg" width="250" height="250" align="left">Army Institute of Technology, Pune (AIT) is an undergraduate engineering college affiliated to the University of Pune. AIT is located at Dighi Hills, Alandi Road area in Pune, Maharashtra, India. Only wards of army personnel are allowed admission. The admission is done through JEE MAINS exam. AIT functions under the aegis of the Army Welfare Education Society (AWES) and has the seniormost officer of the Indian Army, the Chief of Army Staff of the Indian Army (COAS), as the President of its Board of Governors.</p>
+
+</body>
+</html>
